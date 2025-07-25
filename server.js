fce2d69d81ab23fdcc29438ada32998b59841e5a
Joaopaulo55 <joaofranciscovicapaulo55@gmail.com>
2025-07-25 22:58:38 +0100

    ⚠️

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const app = express();
+const Parser = require('rss-parser');
 
 app.use(cors());
 
